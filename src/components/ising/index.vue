179bd877822e23468102640287097e39d6a75c17
wzc11227 <50304256+wzc11227@users.noreply.github.com>
2019-12-03 12:00:30 +0800

    heat-bath
    加入heat-bath方法
    修改函数名使其更统一
    修改不同模式互相调用逻辑

--- src/components/ising/index.vue
+++ src/components/ising/index.vue
@@ -32,6 +32,7 @@
           </el-row>
           <el-row>
             <el-radio v-model="simulateMode" label="metropolis">metropolis</el-radio>
+            <el-radio v-model="simulateMode" label="heat-bath">heat-bath</el-radio>
             <el-radio v-model="simulateMode" label="wolff-cluster">wolff-cluster</el-radio>
           </el-row>
           <el-row>
@@ -50,7 +51,7 @@
             <el-slider
               v-model="spf"
               :min="Number(0)"
-              :max="Number(11)"
+              :max="Number(12)"
               :step="Number(1)"
               id="spf"
               :format-tooltip="formatTooltipSpf"
@@ -58,7 +59,7 @@
           </el-row>
 
           <el-button @click="act2" type="primary">下一步</el-button>
-          <el-button @click="running = true;act3()" type="primary">开始</el-button>
+          <el-button @click="start" type="primary">开始</el-button>
           <el-button @click="go" type="primary">初始化</el-button>
           <el-button @click="stopAnimation" type="danger">停止</el-button>
         </el-card>
@@ -100,7 +101,7 @@ export default {
       running: false, //控制运行
       wikiEnabled: false,//wiki弹窗可见
       aboutEnabled: false,//about弹窗可见
-      spfOptions: [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000],
+      spfOptions: [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000],
       sizeOptions: [
         {
           value: 32,
@@ -128,11 +129,20 @@ export default {
   created() {
     setTimeout(() => {
       this.go();//初始化
-      this.running = true;//初始化结束开始运行
-      this.act3();
+      this.start();
     }, 100);//延迟100ms调用初始化 避免找不到组件
   },
   methods: {
+    start(){
+      this.running = true;
+      let mode = this.simulateMode;
+      if (mode == "metropolis")
+        this.act_metro();
+      else if (mode == "heat-bath")
+        this.act_hb();
+      else if (mode == "wolff-cluster")
+        this.act_wolff();
+    },
     formatTooltipSpf(val) {//格式化spf的slider的tootip
       return this.spfOptions[val];
     },
@@ -151,122 +161,166 @@ export default {
       this.activeIndex = "";
       //this.menuKey=this.menuKey=='1'?'0':'1';
     },
-    act4() {
-      //wolff算法
-      if (this.running && this.simulateMode == "wolff-cluster"){
-        let length = this.length;
-        let pixelMethod = length>100;
-        let stepPF = this.spfOptions[this.spf];
-        let temp = this.temperature;
-        //for (let loop = 0; loop < stepPF; loop ++){
-          let element = parseInt(Math.random() * length * length);
-          // let cluster = [{
-          //   'x': element % length,
-          //   'y': parseInt(element / length)
-          // }];
-          let cluster = new Array(length);
-          for (let i = 0; i < length; i++)
-            cluster[i] = new Array(length).fill(0);
-          let Fold = [{
-            'x': element % length,
-            'y': parseInt(element / length)
-          }];
-          while (Fold.length != 0){
-            let Fnew = [];
-            for (let a = 0; a < Fold.length; a++){
-              let i = Fold[a];
-              for ( let p = -1; p <= 1; p += 2)
-                for ( let q = -1; q <= 1; q += 2){ //遍历neighbor
-                  let j = {};
-                  j.x = (i.x + ( p + q ) / 2 + length) % length;
-                  j.y = (i.y + ( p - q ) / 2 + length) % length;
-                  if (this.arr[i.x][i.y] == this.arr[j.x][j.y] && 
-                  // !cluster.find(point => {
-                  //   return point.x==j.x&&point.y==j.y;
-                  // })
-                  cluster[j.x][j.y] == 0
-                  )//状态相同为同一族群
-                  {
-                    let T = Math.pow(10, temp).toFixed(2);
-                    let prob = Math.exp(-2/T);
-                    if (Math.random() > prob){
-                      Fnew.push(j);
-                      // cluster.push(j);
-                      cluster[j.x][j.y] = 1;
-                    }
-                  }
-                }
+    act_hb() {
+      //heat-bath方法
+      if (this.running){
+        if (this.simulateMode == "heat-bath"){
+          let length = this.length;
+          let pixelMethod = length>100;
+          let stepPF = this.spfOptions[this.spf];
+          let temp = this.temperature;
+          for (let loop = 0; loop < stepPF; loop++){
+            let element = parseInt(Math.random() * length * length);
+            let x = element%length;
+            let y = parseInt(element/length);
+            //window.console.log('x:'+x+' y:'+y);
+            let eup = this.getEnergy(x, y)*this.arr[x][y];
+            let edown = -eup;
+            let T = Math.pow(10, temp).toFixed(2);
+            let pup = Math.exp(-eup/T);
+            let pdown = Math.exp(-edown/T);
+            let probup = pup/(pup+pdown);
+            if (Math.random() < probup){
+              this.arr[x][y] = 1;
+            }else{
+              this.arr[x][y] = -1;
             }
-            Fold = Fnew;
-          }
-          //console.log(cluster);
-          //for( let i = 0; i < cluster.length; i++){
-            for( let i = 0; i < length; i++)
-            for( let j = 0; j < length; j++){
-            //this.arr[cluster[i].x][cluster[i].y] *= -1;
-            if (cluster[i][j] == 1){
-              this.arr[i][j]*=-1;
-              if (pixelMethod)
-                //this.draw(cluster[i].x, cluster[i].y);
-                this.draw(i,j);
-              else
-                //this.draw2(cluster[i].x, cluster[i].y);
-                this.draw2(i,j);
-            }
-              
-          }
-        //}
-        if (pixelMethod)
-          this.cxt.putImageData(this.image, 0, 0); //刷新画布
-      }else if (this.running && this.simulateMode == "metropolis")
-        this.act3();
-      setTimeout(() => {
-        //间隔1ms 递归调用
-        this.act4();
-      }, 1);
-    },
-    act3() {
-      //metropolis方法
-      let firstarr = [{
-        'x': 1,
-        'y': 2
-      },{
-        'x': 2,
-        'y': 3
-      }];
-      let secondarr = {
-        'x': 1,
-        'y': 2
-      };
-      if (this.running && this.simulateMode == 'metropolis') {
-        let length = this.length;
-        let pixelMethod = length>100;
-        let stepPF = this.spfOptions[this.spf];
-        let temp = this.temperature;
-        for (let loop = 0; loop < stepPF ; loop++) {
-          let element = parseInt(Math.random() * length * length);
-          let x = element % length;
-          let y = parseInt(element / length);
-          let energy = this.getEnergy(x, y);
-          let deltaEnergy = energy * -2;
-          let T = Math.pow(10, temp).toFixed(2);
-          let prob = Math.exp(-deltaEnergy / T);
-          if (Math.random() < prob) {
-            this.arr[x][y] *= -1;
             if (pixelMethod)
               this.draw(x, y);
             else
               this.draw2(x, y);
           }
+          if (pixelMethod)
+            this.cxt.putImageData(this.image, 0, 0); //刷新画布
+          setTimeout(() => {
+            //间隔1ms 递归调用
+            this.act_hb();
+          }, 1);
+        }else if (this.simulateMode == "wolff-cluster"){
+          this.act_wolff();
+        }else if (this.simulateMode == "metropolis"){
+          this.act_metro();
         }
-        if (pixelMethod)
-          this.cxt.putImageData(this.image, 0, 0); //刷新画布
-      }else if (this.running && this.simulateMode == "wolff-cluster")
-        this.act4();
-      setTimeout(() => {
-        //间隔1ms 递归调用
-        this.act3();
-      }, 1);
+      }
+    },
+    act_wolff() {
+      //wolff方法
+      if (this.running){
+        if (this.simulateMode == "wolff-cluster"){
+          let length = this.length;
+          let pixelMethod = length>100;
+          let stepPF = this.spfOptions[this.spf];
+          let temp = this.temperature;
+          //for (let loop = 0; loop < stepPF; loop ++){
+            let element = parseInt(Math.random() * length * length);
+            // let cluster = [{
+            //   'x': element % length,
+            //   'y': parseInt(element / length)
+            // }];
+            let cluster = new Array(length);
+            for (let i = 0; i < length; i++)
+              cluster[i] = new Array(length).fill(0);
+            let Fold = [{
+              'x': element % length,
+              'y': parseInt(element / length)
+            }];
+            while (Fold.length != 0){
+              let Fnew = [];
+              for (let a = 0; a < Fold.length; a++){
+                let i = Fold[a];
+                for ( let p = -1; p <= 1; p += 2)
+                  for ( let q = -1; q <= 1; q += 2){ //遍历neighbor
+                    let j = {};
+                    j.x = (i.x + ( p + q ) / 2 + length) % length;
+                    j.y = (i.y + ( p - q ) / 2 + length) % length;
+                    if (this.arr[i.x][i.y] == this.arr[j.x][j.y] && 
+                    // !cluster.find(point => {
+                    //   return point.x==j.x&&point.y==j.y;
+                    // })
+                    cluster[j.x][j.y] == 0
+                    )//状态相同为同一族群
+                    {
+                      let T = Math.pow(10, temp).toFixed(2);
+                      let prob = Math.exp(-2/T);
+                      if (Math.random() > prob){
+                        Fnew.push(j);
+                        // cluster.push(j);
+                        cluster[j.x][j.y] = 1;
+                      }
+                    }
+                  }
+              }
+              Fold = Fnew;
+            }
+            //console.log(cluster);
+            //for( let i = 0; i < cluster.length; i++){
+              for( let i = 0; i < length; i++)
+              for( let j = 0; j < length; j++){
+              //this.arr[cluster[i].x][cluster[i].y] *= -1;
+              if (cluster[i][j] == 1){
+                this.arr[i][j]*=-1;
+                if (pixelMethod)
+                  //this.draw(cluster[i].x, cluster[i].y);
+                  this.draw(i,j);
+                else
+                  //this.draw2(cluster[i].x, cluster[i].y);
+                  this.draw2(i,j);
+              }
+                
+            }
+          //}
+          if (pixelMethod)
+            this.cxt.putImageData(this.image, 0, 0); //刷新画布
+          if (this.simulateMode == "wolff-cluster")
+            setTimeout(() => {
+              //间隔1ms 递归调用
+              this.act_wolff();
+            }, 1);
+        }else if (this.simulateMode == "metropolis"){
+          this.act_metro();
+        }else if (this.simulateMode == "heat-bath"){
+          this.act_hb();
+        }
+        
+      }
+    },
+    act_metro() {
+      //metropolis方法
+      if (this.running) {
+        if (this.simulateMode == 'metropolis'){
+          let length = this.length;
+          let pixelMethod = length>100;
+          let stepPF = this.spfOptions[this.spf];
+          let temp = this.temperature;
+          for (let loop = 0; loop < stepPF ; loop++) {
+            let element = parseInt(Math.random() * length * length);
+            let x = element % length;
+            let y = parseInt(element / length);
+            let energy = this.getEnergy(x, y);
+            let deltaEnergy = energy * -2;
+            let T = Math.pow(10, temp).toFixed(2);
+            let prob = Math.exp(-deltaEnergy / T);
+            if (Math.random() < prob) {
+              this.arr[x][y] *= -1;
+              if (pixelMethod)
+                this.draw(x, y);
+              else
+                this.draw2(x, y);
+            }
+          }
+          if (pixelMethod)
+            this.cxt.putImageData(this.image, 0, 0); //刷新画布
+          if (this.simulateMode == "metropolis")
+            setTimeout(() => {
+              //间隔1ms 递归调用
+              this.act_metro();
+            }, 1);
+        }else if (this.simulateMode == "wolff-cluster"){
+          this.act_wolff();
+        }else if (this.simulateMode == "heat-bath"){
+          this.act_hb();
+        }
+      }
     },
     act2() {
       //分步模拟
@@ -420,22 +474,7 @@ export default {
 
       this.count = count;
 
-      // newArray.forEach(element => {
-      //   let x = element%this.length;
-      //   let y = parseInt(element/this.length);
-      //   //window.console.log('x:'+x+' y:'+y);
-      //   let eup = this.getEnergy(x, y)*this.arr[x][y];
-      //   let edown = -eup;
-      //   let T = this.T;
-      //   let pup = Math.exp(-eup/T);
-      //   let pdown = Math.exp(-edown/T);
-      //   let probup = pup/(pup+pdown);
-      //   if (Math.random() < probup){
-      //     this.arr[x][y] = 1;
-      //   }else{
-      //     this.arr[x][y] = -1;
-      //   }
-      // });
+
     },
     getEnergy(x, y) {
       //获取xy坐标点的能量值
